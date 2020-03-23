a1b19d6b1d510bb46edaa9f3393bbbfa1b7f039b
Joshua Andrew Jourdain <yahwehprovides@joshuas-mbp.attlocal.net>
2020-03-23 15:33:51 -0400

    revert _document.tsx to the medium article to figure out error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import NextDocument from 'next/document'
 import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles'
 export default class Document extends NextDocument {
-  static async getInitialProps (ctx: any) {
+  static async getInitialProps (ctx) {
     const styledComponentSheet = new StyledComponentSheets()
     const materialUiSheets = new MaterialUiServerStyleSheets()
     const originalRenderPage = ctx.renderPage
