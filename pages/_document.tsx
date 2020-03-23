616a59f5a943fb8ef0f773c47d2847cb9438e707
Joshua Andrew Jourdain <yahwehprovides@joshuas-mbp.attlocal.net>
2020-03-23 15:53:01 -0400

    revert back to old way to wait for discussion to unfold

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,16 @@
+//A custom Document is commonly used to augment your application's <html> and <body> tags. This is necessary because Next.js pages skip the definition of the surrounding document's markup.
+//https://nextjs.org/docs/advanced-features/custom-document
+
+//A custom Document can also include getInitialProps for expressing asynchronous server-rendering data requirements.
+// i think that's what the medium article aims to do.
+
+// IMPORTS
 import React from 'react'
 import NextDocument from 'next/document'
 import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles'
+
+//DEFAULT EXPORT
 export default class Document extends NextDocument {
   static async getInitialProps (ctx) {
     const styledComponentSheet = new StyledComponentSheets()
